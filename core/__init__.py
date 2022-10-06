e5b8c1c734bd838aabb9657a03c007da2eaf063a
Robert Rozas <Robert.Rozas@jbtc.com>
2022-10-06 17:38:52 -0300

    now it creates the schema for the table on first run

--- core/__init__.py
+++ core/__init__.py
@@ -4,13 +4,3 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']  = 'sqlite:///simple.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-
-db = SQLAlchemy(app)
-
-from core.modules.data import data_bcg
-
-with app.app_context():
-    print("here")
-    from core.modules.data import data_bcg
-    db.create_all()
-    db.session.commit()
